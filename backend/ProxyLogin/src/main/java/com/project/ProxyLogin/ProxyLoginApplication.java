bbddf484a09228bf68cb1f5b6da7e421e271e33b
gdicecio <giuseppefrancesco98@gmail.com>
2022-11-29 19:00:50 +0100

    proxy + vault

--- backend/ProxyLogin/src/main/java/com/project/ProxyLogin/ProxyLoginApplication.java
+++ backend/ProxyLogin/src/main/java/com/project/ProxyLogin/ProxyLoginApplication.java
@@ -1,19 +1,14 @@
 package com.project.ProxyLogin;
 
-import com.project.ProxyLogin.web.Secret;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.vault.config.Secrets;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.vault.core.VaultOperations;
+import org.springframework.vault.authentication.TokenAuthentication;
+import org.springframework.vault.client.VaultEndpoint;
 import org.springframework.vault.core.VaultTemplate;
 import org.springframework.vault.support.VaultResponse;
-import org.springframework.vault.support.VaultResponseSupport;
+
+import java.net.URI;
+import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class ProxyLoginApplication {
@@ -22,14 +17,23 @@ public class ProxyLoginApplication {
 
 	public static void main(String[] args) {
 
-		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(SecurityConfiguration.class);
-		context.start();
-		SecurityConfiguration securityConfiguration = context.getBean(SecurityConfiguration.class);
-		String passKeystore=securityConfiguration.getPassKeystore();
-		System.out.println("Questa è la passKeystore:"+passKeystore);
-		System.setProperty("server.ssl.key-store-password",passKeystore);
+		VaultTemplate template = null;
+		try {
+			template = new VaultTemplate(VaultEndpoint.from(new URI("http://127.0.0.1:8200")), new TokenAuthentication("hvs.bLXKpXMba7g0IayOm6AbDbHX"));
+		} catch (URISyntaxException e) {
+			e.printStackTrace();
+		}
+
+		VaultResponse response = template.read("kv/keys");
+
+		String pin = response.getData().get("certificate_pin").toString();
+
+		System.setProperty("server.ssl.key-store-password", pin);
+
 		SpringApplication.run(ProxyLoginApplication.class, args);
 	}
+
+
 
 
 }
